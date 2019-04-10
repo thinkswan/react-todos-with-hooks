e1e60777b67b5140ec1c573d8c97019cc47da18b
Graham Swan <thinkswan@gmail.com>
2019-04-10 21:17:46 +0200

    Add ability to add todos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ function Todo({ todo, index }) {
   return <div className="todo">{todo.text}</div>
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (!value) return
+
+    addTodo(value)
+    setValue("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add todo..."
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -21,12 +46,19 @@ function App() {
     }
   ])
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
